ab41958c53dcec4e672b6b44db6d7f0e31839f50
Evan Walters <evan@evanw.uk>
2021-10-26 01:00:49 +0100

    boilerplate done

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	plugins: ['@typescript-eslint'],
+	ignorePatterns: ['*.cjs'],
+	env: {
+		node: true
+	}
+}
