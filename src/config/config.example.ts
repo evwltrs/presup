9870861e63733ef406c3a36314a8733b515bd99e
Evan Walters <evan@evanw.uk>
2021-10-26 18:50:59 +0100

    basic concept done

---
+++ src/config/config.example.ts
@@ -0,0 +1,6 @@
+export const config = {
+	port: 3000, // Port you want to run the server on
+	botToken: 'xxxxxxxxxxx', // Token of the bot - get from https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token
+	guildId: 'xxxxxxxxxxx', // id of the guild you share with the bot
+	userId: 'xxxxxxxxxxx' // id of your user
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,32 @@
-console.log('Hello World')
+import { config } from './config/config'
+
+import { Client, Intents } from 'discord.js'
+import express from 'express'
+
+const app = express()
+const port = config.port
+
+const myIntents = new Intents()
+myIntents.add()
+myIntents.add(Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILDS)
+const client = new Client({ intents: myIntents })
+
+client.on('ready', async () => {
+	console.log(`Logged in as ${client.user!.tag}!`)
+})
+
+async function getPresence() {
+	const user = await client.users.fetch(config.userId)
+	const guild = await client.guilds.fetch(config.guildId)
+	const member = await guild.members.fetch(user)
+	return member.presence!.activities
+}
+
+app.get('/', async function (req, res) {
+	res.send(await getPresence())
+})
+
+app.listen(port, () => {
+	console.log(`Listening at http://localhost:${port}`)
+})
+client.login(config.botToken)
